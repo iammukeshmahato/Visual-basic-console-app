0cd248dd03be4eb3cc045500a38246e55b1059d7
Niruta Dahal <nirutadahal502@gmail.com>
2022-10-14 14:22:56 +0545

    Adding folder to find the sum of first n number

---
+++ sum of first n natural number/sum of first n natural number/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Function sumOfNaturalNumber(ByVal n As Integer)
+        Dim sum As Integer = 0
+        For i = 1 To n
+            sum += i
+        Next
+        Return sum
+    End Function
+    Sub Main()
+        Dim input As Integer
+        Console.Write("Enter any number: ")
+        input = Console.ReadLine()
+        Console.WriteLine("The sum of first " & input & " natural number is " & sumOfNaturalNumber(input))
+        Console.ReadKey()
+    End Sub
+
+End Module
